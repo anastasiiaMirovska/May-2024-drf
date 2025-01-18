e3b58f0889f054546ee17037ede0de2959f6a426
Anastasiia Mirovska <anastasiia.mirovska@gmail.com>
2025-01-18 19:39:11 +0200

    Some changes

--- backend/apps/chat/consumers.py
+++ backend/apps/chat/consumers.py
@@ -1,12 +1,18 @@
+import datetime
+
 from channels.db import database_sync_to_async
 from djangochannelsrestframework.decorators import action
 from djangochannelsrestframework.generics import GenericAsyncAPIConsumer
+
+from django.db.models import F
+
+from apps.chat.models import ChatMessageModel, ChatRoomModel
 
 
 class ChatConsumer(GenericAsyncAPIConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.room_name = None
+        self.room = None
         self.user_name = None
 
     async def connect(self):
@@ -14,16 +20,25 @@ class ChatConsumer(GenericAsyncAPIConsumer):
             return await self.close()
 
         await self.accept()
-        self.room_name = self.scope['url_route']['kwargs']['room']
+        room_name = self.scope['url_route']['kwargs']['room']
+        self.room, _ = await ChatRoomModel.objects.aget_or_create(name=room_name)
         self.user_name = await self.get_profile_name()
-        print(self.room_name)
-        print(self.user_name)
         await self.channel_layer.group_add(
-            self.room_name,
+            self.room.name,
             self.channel_name
         )
+
+        messages = await self.get_last_five_messages()
+
+        for name, text in messages:
+            await self.sender({
+                'message': text,
+                'user': name,
+                "request_id": str(datetime.datetime.now())
+            })
+
         await self.channel_layer.group_send(
-            self.room_name,
+            self.room.name,
             {
                 'type': 'sender',
                 'message': f'{self.user_name} connected to chat'
@@ -35,8 +50,9 @@ class ChatConsumer(GenericAsyncAPIConsumer):
 
     @action()
     async def send_message(self, data, request_id, action):
+        await ChatMessageModel.objects.acreate(text=data, user=self.scope['user'], room=self.room)
         await self.channel_layer.group_send(
-            self.room_name,
+            self.room.name,
             {
                 'type': 'sender',
                 'message': data,
@@ -51,3 +67,7 @@ class ChatConsumer(GenericAsyncAPIConsumer):
         user = self.scope['user']
         return user.profile.name
 
+    @database_sync_to_async
+    def get_last_five_messages(self):
+        res = self.room.messages.annotate(name=F('user__profile__name')).values('text', 'name').order_by('-id')[:5]
+        return reversed([(message['name'], message['text'])for message in res])
--- backend/apps/chat/models.py
+++ backend/apps/chat/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+UserModel = get_user_model()
+
+class ChatRoomModel(models.Model):
+    class Meta:
+        db_table = 'chat_rooms'
+
+    name = models.CharField(max_length=50)
+
+
+class ChatMessageModel(models.Model):
+    class Meta:
+        db_table = 'chat_messages'
+
+    text = models.TextField()
+    room = models.ForeignKey(ChatRoomModel, on_delete=models.CASCADE, related_name='messages')
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name='messages')
+
